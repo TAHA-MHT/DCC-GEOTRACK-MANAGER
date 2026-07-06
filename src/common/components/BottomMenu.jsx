cde91f5487bfb5da2de025f0fd01493576412d53
TAHA-MHT <tahadiamondben@gmail.com>
2026-07-06 16:28:07 +0100

    Remove unused DashboardIcon import
    Removed unused DashboardIcon import from BottomMenu.jsx.

--- src/common/components/BottomMenu.jsx
+++ src/common/components/BottomMenu.jsx
@@ -15,7 +15,6 @@ import {
 import DescriptionIcon from '@mui/icons-material/Description';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MapIcon from '@mui/icons-material/Map';
-import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonIcon from '@mui/icons-material/Person';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { sessionActions } from '../../store';
